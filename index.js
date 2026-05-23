06e6231fa557df8b342e13e72b543295c10f5364
OptimizerX <tawanbryan126@gmail.com>
2026-05-23 14:42:22 -0300

    fix render

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Bot online!");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("🌐 Web server online");
+});
 const {
     Client,
     GatewayIntentBits,
